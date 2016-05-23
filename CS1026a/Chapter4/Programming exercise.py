784d69eb75ec6b8a24fb34629792a2445491afe1
yijunhhhe <yijunhhhe@gmail.com>
2016-05-23 12:03:36 -0400

    some works

--- CS1026a/Chapter4/Programming exercise.py
+++ CS1026a/Chapter4/Programming exercise.py
@@ -13,7 +13,33 @@ print("")
 # Every second letter of the string
 print(string[1])
 
-# The string, with all vowels replaced by an underscore
-for char in string:
-    if char == "A" or "E" or "I" or "O" or "U":
-        
+
+
+#Translate pseudocode for randomly permuting the characters in a string
+from random import randint
+first = ""
+middle = ""
+last = ""
+word = input("Please enter a word: ")
+for numberOfTime in range(len(word)):
+    i = randint(0,len(word)-1)
+    l = i + 1
+    if i != len(word)-1:
+        j = randint(l,len(word)-1)
+for a in range(0,i-1):
+    first = first + word[a]
+for b in range(i,j-1):
+    middle = middle + word[b]
+for c in range(j,len(word)):
+    last = last + word[c]
+
+print(first + word[j-1] + middle + word[i-1] + last)
+
+
+# Read a word and prints the word in reverse
+word = input("Please enter a word: ")
+for i in range(len(word)-1,-1,-1):
+    print(word[i], end="")
+
+
+
