31ac6533bfecf6f3ab36bcbeae0f8a04da3cc651
Yijun He Surface <yijunhhhe@gmail.com>
2016-06-02 18:25:49 -0400

    assignment2

--- CS1026a/yhe362_Assign2.py
+++ CS1026a/yhe362_Assign2.py
@@ -40,7 +40,7 @@ def computeScoreOfTheTweet(tweetsWordList,keywordList):
     sentimentValues = 0
     # Obtain every word in the tweets word list
     for word in tweetsWordList:
-        word = word.rstrip(".?',!/")
+        word = word.strip(".?',!/")
         # find out if there are any words from tweets line in keywords list
         if word in keywordList:
             # Avoid Value Error
@@ -55,82 +55,85 @@ def computeScoreOfTheTweet(tweetsWordList,keywordList):
         scoreOfTheTweet = sentimentValues/numOfKeywords
         return scoreOfTheTweet
 
-# Receive the input of keyword file's name
-#keywordFileName = input("Please enter the name of keyword file: ")
+def main():
+    # Receive the input of keyword file's name
+    #keywordFileName = input("Please enter the name of keyword file: ")
 
-# Make sure keywords file exists
-try:
-    keywordFile = open("keywords.txt", "r")
-except IOError as fileNameError:
-    print(fileNameError)
-    sys.exit(0)
+    # Make sure keywords file exists
+    try:
+        keywordFile = open("keywords.txt", "r")
+    except IOError as fileNameError:
+        print(fileNameError)
+        sys.exit(0)
 
-# Read lines from Keywords file and store it in keyword list
-line = keywordFile.readline()
-keywordList = []
-while line != "":
-    wordList = line.split(",")
-    for word in wordList:
-        word = word.rstrip()
-        keywordList.append(word)
+    # Read lines from Keywords file and store it in keyword list
     line = keywordFile.readline()
+    keywordList = []
+    while line != "":
+        wordList = line.split(",")
+        for word in wordList:
+            word = word.rstrip()
+            keywordList.append(word)
+        line = keywordFile.readline()
 
-# Obtain the name of the tweets file
-#tweetFileName = input("Please enter the name of the tweets file: ")
+    # Obtain the name of the tweets file
+    #tweetFileName = input("Please enter the name of the tweets file: ")
 
-# Make sure tweets file exists
-try:
-    tweetFile = open("tweets.txt","r")
-except IOError as fileNameError:
-    print(fileNameError)
-    sys.exit(0)
+    # Make sure tweets file exists
+    try:
+        tweetFile = open("tweets.txt","r")
+    except IOError as fileNameError:
+        print(fileNameError)
+        sys.exit(0)
 
-# Define different time zone as lists
-pacific = []
-mountain = []
-central = []
-eastern = []
+    # Define different time zone as lists
+    pacific = []
+    mountain = []
+    central = []
+    eastern = []
 
-# Read line from tweets file
-line = tweetFile.readline()
-count = 1
-while line != "":
-    tweetsWordList = line.split(" ")
-    # Compute the score of each tweet
-    scoreOfTheTweet = computeScoreOfTheTweet(tweetsWordList,keywordList)
-    # find the time zone the tweet was in
-    timeZone = determineTimeZone(tweetsWordList)
-    print(count, scoreOfTheTweet)
-    print(timeZone)
-    # Ignore the tweet without keywords and outside time zone
-    # add the score of the tweet to corresponding time zone
-    if scoreOfTheTweet != None and timeZone != "":
-        if timeZone == "Pacific":
-            pacific.append(scoreOfTheTweet)
-        elif timeZone == "Mountain":
-            mountain.append(scoreOfTheTweet)
-        elif timeZone == "Central":
-            central.append(scoreOfTheTweet)
-        else:
-            eastern.append(scoreOfTheTweet)
+    # Read line from tweets file
     line = tweetFile.readline()
-    count = count + 1
+    count = 1
+    while line != "":
+        tweetsWordList = line.split(" ")
+        # Compute the score of each tweet
+        scoreOfTheTweet = computeScoreOfTheTweet(tweetsWordList,keywordList)
+        # find the time zone the tweet was in
+        timeZone = determineTimeZone(tweetsWordList)
+        print(count, scoreOfTheTweet)
+        print(timeZone)
+        # Ignore the tweet without keywords and outside time zone
+        # add the score of the tweet to corresponding time zone
+        if scoreOfTheTweet != None and timeZone != "":
+            if timeZone == "Pacific":
+                pacific.append(scoreOfTheTweet)
+            elif timeZone == "Mountain":
+                mountain.append(scoreOfTheTweet)
+            elif timeZone == "Central":
+                central.append(scoreOfTheTweet)
+            else:
+                eastern.append(scoreOfTheTweet)
+        line = tweetFile.readline()
+        count = count + 1
 
-try:
-    scoreOfPacific = computeRegionSentiment(pacific)
-    scoreOfMountain = computeRegionSentiment(mountain)
-    scoreOfCentral = computeRegionSentiment(central)
-    scoreOfEastern = computeRegionSentiment(eastern)
-except ValueError:
-    pass
+    try:
+        scoreOfPacific = computeRegionSentiment(pacific)
+        scoreOfMountain = computeRegionSentiment(mountain)
+        scoreOfCentral = computeRegionSentiment(central)
+        scoreOfEastern = computeRegionSentiment(eastern)
+    except ValueError:
+        pass
 
-print("The happiness score in pacific is: {}, and the number of tweets is: {}\n"
-      "The happiness score in Mountain is: {}, and the numebr of tweets is: {}\n"
-      "The happiness score in Central is: {}, and the numebr of tweets is: {}\n"
-      "The happiness score in Eastern is: {}, and the numebr of tweets is: {}\n"
-      .format(scoreOfPacific, len(pacific), scoreOfMountain, len(mountain),
-              scoreOfCentral, len(central), scoreOfEastern, len(eastern) ))
+    print("The happiness score in pacific is: {}, and the number of tweets is: {}\n"
+          "The happiness score in Mountain is: {}, and the numebr of tweets is: {}\n"
+          "The happiness score in Central is: {}, and the numebr of tweets is: {}\n"
+          "The happiness score in Eastern is: {}, and the numebr of tweets is: {}\n"
+          .format(scoreOfPacific, len(pacific), scoreOfMountain, len(mountain),
+                  scoreOfCentral, len(central), scoreOfEastern, len(eastern) ))
 
-keywordFile.close()
-tweetFile.close()
+    keywordFile.close()
+    tweetFile.close()
+
+main()
 
