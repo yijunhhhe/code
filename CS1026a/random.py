5c884c9104239c9989233cbf6fc8ffa9dd7f819f
Yijun He Home <yijunhhhe@gmail.com>
2016-06-09 16:32:17 -0400

    lab

--- CS1026a/random.py
+++ CS1026a/random.py
@@ -1,27 +1,8 @@
-inputFile = open("random.txt", "r",encoding="utf-8", errors='ignore')
-wordDict = {}
-for line in inputFile:
-    lineList = line.split()
-    for word in lineList:
-        word = word.rstrip()
-        if word not in wordDict:
-            wordDict[word] = 1
-        else :
-            wordDict[word] += 1
-
-
-
-value = list(wordDict.values())
-value.sort()
-value.reverse()
-newValue = value[0:100]
-print(newValue)
-count = 0
-for i in range(len(newValue)):
-    if newValue[i] == newValue[i-1]:
-        continue
-    for key in wordDict:
-        if wordDict[key] == newValue[i]:
-            count += 1
-            print(count,key)
-
+class Cream:
+    def __init__(self):
+        self.value = "um"
+class Coffee:
+    def __init__(self):
+        self.value = "Y"
+    def __add__(self, cream):
+        return self.value + cream.value
