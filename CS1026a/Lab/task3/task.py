d6f4ed5633e4135517f65e04c8c331a3f129f199
Yijun He Home <yijunhhhe@gmail.com>
2016-06-09 12:28:32 -0400

    sth

---
+++ CS1026a/Lab/task3/task.py
@@ -0,0 +1,10 @@
+sentence ="I had such a horrible day. It was awful so bad sigh. It could not have been worse but actually though "\
+          +"such a terrible horrible awful bad day."
+
+makeItHappy ={"horrible":"amazing","bad":"good","awful":"awesome","worse":"better","terrible":"great"}
+
+#place your code here
+
+print(newString)
+
+
