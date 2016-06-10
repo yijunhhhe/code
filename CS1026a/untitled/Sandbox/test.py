191ab111e6d09739c6f0b446b30c329057c49f55
Yijun He Home <yijunhhhe@gmail.com>
2016-06-10 11:37:59 -0400

    some update

--- CS1026a/untitled/Sandbox/test.py
+++
@@ -1,6 +0,0 @@
-from Car import *
-
-myHybrid = Car(50)
-myHybrid.addGas(20)
-myHybrid.drive(100)
-print(myHybrid.getGasLevel())
--- CS1026a/untitled1/lesson1/task3/task.py
+++
@@ -1,7 +0,0 @@
-# TODO: type solution here
-#place your code here
-
-coffee = Coffee()
-cream= Cream()
-
-print(coffee+cream)
