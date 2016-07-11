9d00eda616895f709dccf6b19f3c95c69e246260
Home <yijunhhhe@gmail.com>
2016-07-11 11:45:44 +0800

    assignment2

--- CS1027/src/Assignment2/MazeSolver.java
+++ CS1027/src/Assignment2/MazeSolver.java
@@ -16,19 +16,39 @@ public class MazeSolver {
 		Maze maze = new Maze(mazeFileName);
 		
 		Hexagon startHexagon = new Hexagon(Hexagon.HexType.START);
-		
+		int stepCounter = 0, hexagonOnStack = 0;
 		ArrayStack<Hexagon> stack = new ArrayStack<Hexagon>();
 		stack.push(maze.getStart());
-		hexagonOnStack++;
-		
-		int stepCounter = 0, hexagonOnStack = 0;
+		hexagonOnStack++;		
 		Hexagon popHexagon = null;
+		Hexagon neighbor;
 		while(!stack.isEmpty()){
 			popHexagon = stack.pop();
 			stepCounter++;
 			hexagonOnStack--;
+			if (popHexagon.isEnd()){
+				popHexagon.setProcessed();
+				break;
+			}else{
+				for(int i = 0; i < 6; i++){
+					neighbor = popHexagon.getNeighbour(i);
+					if (neighbor == null){
+						continue;
+					} else if (!neighbor.isWall() && neighbor.isUnvisited()){
+						stack.push(neighbor);
+						neighbor.setPushed();
+						hexagonOnStack++;
+						System.out.println(neighbor);
+					}
+				}
+			}
+			popHexagon.setProcessed();
+			maze.repaint();
 		}
-		
+		if(popHexagon.isEnd()){
+			System.out.println("Number of steps that it took to finish: " + stepCounter);
+			System.out.println("Number of tiles still on the stack: " + hexagonOnStack);
+		}
 			
 	}
 
