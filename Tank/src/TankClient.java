dc06308af9db65f7ba06b1e3bf05f7c186b67d15
Home <yijunhhhe@gmail.com>
2016-08-15 13:56:00 -0400

    tankwar0.3

--- Tank/src/TankClient.java
+++ Tank/src/TankClient.java
@@ -3,6 +3,14 @@ import java.awt.event.*;
 
 public class TankClient extends Frame {
 
+	public void paint(Graphics g){
+		Color c = g.getColor();	
+		g.setColor(Color.red);
+		g.fillOval(50, 50, 30, 30);
+		g.setColor(c);
+	
+	}
+	
 	public void launchFrame(){
 		this.setLocation(200,100);
 		this.setSize(800, 600);
@@ -13,6 +21,7 @@ public class TankClient extends Frame {
 			}
 		});
 		this.setResizable(false);
+		this.setBackground(Color.GREEN);
 		setVisible(true);
 	}
 	
